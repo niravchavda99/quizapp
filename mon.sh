b6cc3482d330d7d87abed775b69bbc63e8cc3d69
Nirav Chavda <nmchavda99@gmail.com>
2021-10-27 22:47:13 +0530

    Bash Script For Ubuntu

--- mon.sh
+++ mon.sh
@@ -1,18 +1,17 @@
+#!/bin/bash
 # Move to that directory
 
 # cd `git rev-parse --show-toplevel`
-cd WEB-INF/classes
-pwd
-
+cd WEB-INF/classes/
 
 
 # Compile all files
 
-STR=sudo cat /etc/environment | grep CLASSPATH
-echo $STR
-# javac -cp $CLASSPATH models/*.java
-# javac -cp $CLASSPATH utils/*.java
-# javac -cp $CLASSPATH controllers/*.java
+STR=`set | grep CLASSPATH`
+pth=`cut -c 11- <<< $STR`
+javac -cp $pth models/*.java
+javac -cp $pth utils/*.java
+javac -cp $pth controllers/*.java
 
 # Restart Tomcat
-# sudo systemctl restart tomcat
+sudo systemctl restart tomcat
